3bcbae23d6e112dacf703be30e54c53e41dfa4a0
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-17 14:55:29 +0500

    Task3

---
+++ Tyuiu.StoletovNA.Sprint4.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.StoletovNA.Sprint4.Task3.V9.Lib
+{
+    public class DataService : ISprint4Task3V9 {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int cols = array.GetLength(1);
+
+            int summ = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j == 2)
+                    {
+                        summ += array[i, j];
+                    }
+                }
+            }
+            return summ;
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint4.Task3.V9.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.StoletovNA.Sprint4.Task3.V9.Lib;
+namespace Tyuiu.StoletovNA.Sprint4.Task3.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = { { 9, 6, 4, 5, 3 }, 
+                             { 7, 4, 7, 5, 3 }, 
+                             { 8, 5, 9, 9, 3 }, 
+                             { 7, 8, 7, 9, 3 }, 
+                             { 3, 7, 3, 7, 7 } };
+            int wait = 30;
+            int res = ds.Calculate(array);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
