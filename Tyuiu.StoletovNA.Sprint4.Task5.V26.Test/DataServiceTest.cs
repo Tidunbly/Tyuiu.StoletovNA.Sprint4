7c7c6321f54635e74b6d1916bc33be05a1927311
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-26 20:13:20 +0500

    Task5

---
+++ Tyuiu.StoletovNA.Sprint4.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.StoletovNA.Sprint4.Task5.V26.Lib;
+namespace Tyuiu.StoletovNA.Sprint4.Task5.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = { { -4, -3, 2, 1, -2 },
+                              { -3, -2, 1, 2, 4 },
+                              { 4, 2, 1, 2, -3 },
+                              { 3, 2, 1, -2, -1 },
+                              { -2, 2, 1, -3, -2 } };
+
+            int[,] waitMatrix = { { -4, -3, 1, 1, -2 },
+                              { -3, -2, 1, 1, 1 },
+                              { 1, 1, 1, 1, -3 },
+                              { 1, 1, 1, -2, -1 },
+                              { -2, 1, 1, -3, -2 } };
+
+            int[,] resMatrix = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(resMatrix, waitMatrix);
+
+        }
+    }
+}
--- Tyuiu.StoletovNA.Sprint4.Task5.V26/Program.cs
+++ Tyuiu.StoletovNA.Sprint4.Task5.V26/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.StoletovNA.Sprint4.Task4.V24.Lib;
-namespace Tyuiu.StoletovNA.Sprint4.Task4.V24
+﻿using Tyuiu.StoletovNA.Sprint4.Task5.V26.Lib;
+namespace Tyuiu.StoletovNA.Sprint4.Task5.V26
 {
     class Program
     {
@@ -9,23 +9,25 @@ namespace Tyuiu.StoletovNA.Sprint4.Task4.V24
             //Длинна строки 75 символов
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Двумерные массивы. (ввод с клавиатуры)                                  *");
-            Console.WriteLine("* Задание #4                                                              *");
-            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Двумерные массивы. (генератор случайных чисел)                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
             Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
             
-            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный значения*");
-            Console.WriteLine("*ями с клавиатуры в диапазоне от 5 до 9. Заменить четные элементы массива *");
-            Console.WriteLine("* на 1.                                                                   *");
+            Console.WriteLine("*Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случайны*");
+            Console.WriteLine("*ыми значениями в диапазоне от -4 до 4. Заменить положительные элементы на*");
+            Console.WriteLine("*а 1.                                                                     *");
 
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            
+            DataService ds = new DataService();
+            Random rand = new Random();
+
             int rows = 5;
             int cols = 5;
             int[,] matrix = new int[rows, cols];
@@ -34,19 +36,7 @@ namespace Tyuiu.StoletovNA.Sprint4.Task4.V24
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    Console.WriteLine($"Введите {i},{j} элемент массива: ");
-                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Массив: ");
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    Console.WriteLine(matrix[i, j] + " ");
-                    Console.WriteLine();
+                    matrix[i, j] = rand.Next(-4, 4);    
                 }
             }
 
@@ -58,18 +48,20 @@ namespace Tyuiu.StoletovNA.Sprint4.Task4.V24
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            DataService ds = new DataService();
+            Console.WriteLine("Массив: ");
 
             var result = ds.Calculate(matrix);
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    Console.Write(result[i, j] + " ");
+                    Console.Write(matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
-            
+
+
+
             Console.ReadKey();
         }
     }
