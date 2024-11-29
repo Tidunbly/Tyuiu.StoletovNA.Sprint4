63ed3002270436b4898e18fc56593c01290f2105
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-29 22:45:22 +0500

    Task7

---
+++ Tyuiu.StoletovNA.Sprint4.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.StoletovNA.Sprint4.Task7.V24.Lib;
+namespace Tyuiu.StoletovNA.Sprint4.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string str = "651827384219648";
+            int rows = 5;
+            int cols = 3;
+            
+            var res = ds.Calculate(rows, cols, str);
+            var wait = 1179648;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint4.Task7.V24/Program.cs
@@ -0,0 +1,64 @@
+﻿using Tyuiu.StoletovNA.Sprint4.Task7.V24.Lib;
+namespace Tyuiu.StoletovNA.Sprint4.Task7.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Добавление к решению итоговых проектов по спринту                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Дана строка из одноразрядных цифр. Преобразуйте ее в матрицу 5 на 3 и под*");
+            Console.WriteLine("*дсчитайте произведение четных чисел.                                     *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int n = 5;
+            int m = 3;
+
+            int[,] matrix = new int[n, m];
+
+            string str = "651827384219648";
+
+            int index = 0;
+
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(str[index] + " ");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(n, m, str);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
